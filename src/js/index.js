967aef872c708ebd3956feadba20250878a3fa54
NKMillsaps <nkmillsaps@gmail.com>
2020-05-09 20:24:53 +0000

    created new pages webbBio navbar footer. they are not connected to main page yet

--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,9 @@ import "../styles/index.scss";
 //import your own components
 import { Home } from "./component/home.js";
 import { MainPage } from "./component/mainPage.js";
+import { webbBio } from "./component/webbBio.js";
+import { navBar } from "./component/navBar.js";
+import { footer } from "./component/footer.js";
 
 //render your react application
 ReactDOM.render(<MainPage />, document.querySelector("#app"));
